964e6d2b08fa38bb9f7cd0d4a8a1abeb4f58d955
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2024-01-01 17:43:56 +0000

    Game into methods, no class, shows _ on screen

--- Main.py
+++ Main.py
@@ -2,93 +2,154 @@ import random
 import pygame
 
 
-class GUI:
-    def __init__(self):
-        self.surface = None
-        self.game = Game()
+def Play():
+    pygame.init()
 
-    def initialise(self):
-        pygame.init()
-        self.surface = pygame.display.set_mode((800, 800))
-        font = pygame.font.SysFont('americantypewriter', 30)
-        clock = pygame.time.Clock()
-        pygame.display.set_caption('Hangman')
-        background_colour = (0, 0, 0)
-        running = True
-        self.surface.fill(background_colour)
+    surface = pygame.display.set_mode((800, 800))
+    font = pygame.font.SysFont('americantypewriter', 30)
+    clock = pygame.time.Clock()
+
+    user_text = ''
+    output = []
+    bad_guess = 0
+    round_val = 0
+    x_coord = 300
+    word_length_num = "5"
+    new_word = word_length(word_length_num)
+    score_array = []
+    score_rect_array = []
+    for _ in new_word:
+        output.append("_")
+        score = font.render('_', True, (255, 255, 255))
+        score_rect = score.get_rect()
+        score_rect.center = (x_coord, 500)
+        x_coord += 50
+        score_array.append(score)
+        score_rect_array.append(score_rect)
+        print(score_array)
+
+
+
+    background_colour = (0, 0, 0)
+    input_rect = pygame.Rect(350, 600, 50, 32)
+    color_active = pygame.Color(255, 255, 255)
+    color_passive = pygame.Color(200, 200, 200)
+    color = color_passive
+
+    pygame.display.set_caption('Hangman')
+    surface.fill(background_colour)
+    pygame.display.flip()
+
+    running = True
+    active = False
+
+    while running:
+        while round_val < 5:
+            surface.blit(score_array[round_val], score_rect_array[round_val])
+            round_val += 1
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_rect.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    user_text = user_text[:-1]
+                elif event.key == pygame.K_RETURN:
+                    text = user_text
+                    user_text = ' '
+                else:
+                    user_text += event.unicode
+
+        if active:
+            color = color_active
+        else:
+            color = color_passive
+
+        pygame.draw.rect(surface, color, input_rect)
+        text_surface = font.render(user_text, True, (0, 0, 0))
+        surface.blit(text_surface, (input_rect.x, input_rect.y))
+        input_rect.w = max(100, text_surface.get_width() + 10)
         pygame.display.flip()
-        user_text = ''
-        input_rect = pygame.Rect(350, 600, 50, 32)
-        color_active = pygame.Color(255, 255, 255)
-        color_passive = pygame.Color(200, 200, 200)
-        color = color_passive
-        active = False
-        self.game.start()
-
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if input_rect.collidepoint(event.pos):
-                        active = True
-                    else:
-                        active = False
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_BACKSPACE:
-                        user_text = user_text[:-1]
-                    elif event.key == pygame.K_RETURN:
-                        text = user_text
-                        user_text = ' '
-                    else:
-                        user_text += event.unicode
-
-            if active:
-                color = color_active
-            else:
-                color = color_passive
-
-            pygame.draw.rect(self.surface, color, input_rect)
-            text_surface = font.render(user_text, True, (0, 0, 0))
-            self.surface.blit(text_surface, (input_rect.x, input_rect.y))
-            input_rect.w = max(100, text_surface.get_width() + 10)
-            pygame.display.flip()
-            clock.tick(60)
+        clock.tick(60)
 
         pygame.display.update()
 
-    @staticmethod
-    def text_int(text, font, x, y):
-        letter = font.render(text, True, (255, 255, 255))
-        letter_rect = letter.get_rect()
-        letter_rect.center = (x, y)
-        return letter, letter_rect
 
-    def display_empty_letter(self, output):
-        location = 100
-        for z in output:
-            text, text_rect = self.each_display(z)
-            text_rect.center = (location, 500)
-            location += 50  # Adjust the spacing between letters
-            self.surface.blit(text, text_rect)  # Blit the text onto the main surface
-        pygame.display.update()
-
-    def each_display(self, x):
-        font = pygame.font.SysFont('americantypewriter', 30)
-        text = font.render(x, True, (255, 255, 255))
-        text_rect = text.get_rect()
-        return text, text_rect
+def word_length(word_length):
+    new_word = 0
+    if word_length == "3":
+        new_word = three_letter()
+    elif word_length == "4":
+        new_word = four_letter()
+    elif word_length == "5":
+        new_word = five_letter()
+    else:
+        print("Error")
+        exit()
+    return new_word  # Returns the chosen word
 
 
-class Game:
+def text_int(text1, font1, x, y):
+    letter = font1.render(text1, True, (255, 255, 255))
+    letter_rect = letter.get_rect()
+    letter_rect.center = (x, y)
+    return letter, letter_rect
+
+
+"""def display_empty_letter(output):
+    location = 100
+    play = Play()
+    for z in output:
+        text2, text_rect = each_display(z)
+        text_rect.center = (location, 500)
+        location += 50  # Adjust the spacing between letters
+        play.surface.blit(text2, text_rect)  # Blit the text onto the main surface
+    pygame.display.update()"""
+
+
+def each_display(x):
+    font2 = pygame.font.SysFont('americantypewriter', 30)
+    text3 = font2.render(x, True, (255, 255, 255))
+    text_rect = text3.get_rect()
+    return text3, text_rect
+
+
+def three_letter():
+    words = ["art", "bag", "cat", "dig", "eye", "fox", "god", "him", "inn", "jam", "kit", "leg", "mat", "nip",
+             "oil", "pin", "quo", "rid", "sew", "top", "urn", "van", "win", "yap", "zoo"]
+    val = random.randrange(0, (len(words) - 1))
+    return words[val]
+
+
+def four_letter():
+    words = ["able", "bank", "crib", "dart", "echo", "from", "grab", "high", "jump", "kill", "lime", "nope", "open",
+             "pony", "quip", "rose", "stew", "tear", "ulna", "view", "wear", "year", "zero", "zoom"]
+    val = random.randrange(0, (len(words) - 1))
+    return words[val]
+
+
+def five_letter():
+    words = ["acorn", "broom", "blaze", "break", "beach", "cried", "climb", "court", "diary", "dairy", "echos",
+             "flame", "gourd", "hired", "index", "itchy", "jokes", "knock", "kayak", "limbo",
+             "marks", "nippy", "newly", "naval", "novel", "nerve", "naked", "oxbow", "ovals" "owned", "point",
+             "prowl", "pants", "proud", "quits", "quilt", "quart", "rants", "round", "ranch", "stink", "stick",
+             "store", "turnt", "tombs", "unify", "ulcer", "upset", "under", "views", "voice", "wound", "yours",
+             "zooms"]
+    val = random.randrange(0, (len(words) - 1))
+    return words[val]
+
+
+"""class Game:
     # Initialise the Word class
     def __init__(self):
-        self.goo = GUI()
-        self.word = Word()
         self.output = []
-        self.surface = self.goo.surface
 
     # Runs the game
     def start(self):
@@ -96,7 +157,6 @@ class Game:
         word_length = "5"
         new_word = self.word_length(word_length)
         for _ in new_word:
-            self.goo.display_empty_letter(self.output)
             self.output.append("_")
         while "_" in self.output:
             guess = input("Please guess a letter: ")
@@ -120,18 +180,6 @@ class Game:
         print("The word was " + new_word)
 
     # Sets the length of the word required
-    def word_length(self, word_length):
-        new_word = 0
-        if word_length == "3":
-            new_word = self.word.three_letter()
-        elif word_length == "4":
-            new_word = self.word.four_letter()
-        elif word_length == "5":
-            new_word = self.word.five_letter()
-        else:
-            print("Error")
-            exit()
-        return new_word  # Returns the chosen word
 
     @staticmethod
     def guess(letter):
@@ -162,33 +210,8 @@ class Game:
             return 1
 
 
-class Word:
-    @staticmethod
-    def three_letter():
-        words = ["art", "bag", "cat", "dig", "eye", "fox", "god", "him", "inn", "jam", "kit", "leg", "mat", "nip",
-                 "oil", "pin", "quo", "rid", "sew", "top", "urn", "van", "win", "yap", "zoo"]
-        val = random.randrange(0, (len(words) - 1))
-        return words[val]
-
-    @staticmethod
-    def four_letter():
-        words = ["able", "bank", "crib", "dart", "echo", "from", "grab", "high", "jump", "kill", "lime", "nope", "open",
-                 "pony", "quip", "rose", "stew", "tear", "ulna", "view", "wear", "year", "zero", "zoom"]
-        val = random.randrange(0, (len(words) - 1))
-        return words[val]
-
-    @staticmethod
-    def five_letter():
-        words = ["acorn", "broom", "blaze", "break", "beach", "cried", "climb", "court", "diary", "dairy", "echos",
-                 "flame", "gourd", "hired", "index", "itchy", "jokes", "knock", "kayak", "limbo",
-                 "marks", "nippy", "newly", "naval", "novel", "nerve", "naked", "oxbow", "ovals" "owned", "point",
-                 "prowl", "pants", "proud", "quits", "quilt", "quart", "rants", "round", "ranch", "stink", "stick",
-                 "store", "turnt", "tombs", "unify", "ulcer", "upset", "under", "views", "voice", "wound", "yours",
-                 "zooms"]
-        val = random.randrange(0, (len(words) - 1))
-        return words[val]
-
-
 if __name__ == '__main__':
     game = Game()
-    game.start()
+    game.start() """
+
+Play()
